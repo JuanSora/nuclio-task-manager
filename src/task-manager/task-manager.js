fb3a883f0d21f967b7290eada7ea497b34f9be67
Ion Sora <soraioniosif@gmail.com>
2021-12-15 18:24:11 +0100

    Acabado la logina de la aplicacion

--- src/task-manager/task-manager.js
+++ src/task-manager/task-manager.js
@@ -2,19 +2,46 @@ import {React, useState } from 'react'
 
 
 function TaskManager() {
+    const[title, setTitle]= useState('');
+    const [tasks, setTasks]=useState(['Do dishes', 'Buy Milk']);
+
+
+
     return (
         <div>
             <h1>To do list</h1>
-            <p>Pending tasks</p>
-            <p>Completed tasks</p>
-            <form>
+            <p>Pending tasks: {tasks.length}</p>
+            
+            <form onSubmit={(event) => {event.preventDefault();
+            setTasks([title, ...tasks]);
+            setTitle('');
+            }}>
             <input
-                 type='text'
+                value={title}
+                onChange={(event) => setTitle(event.target.value)}
+                type='text'
             />
             <button type='submit'>
-                Añadir tarea
+                Add Task
             </button>
             </form>
+            <ul>
+            {tasks.map(task =>{
+                return <li onClick={() => {
+                   setTasks(tasks.filter((t)  => t != task)); 
+                }}>{task}</li>
+            })}
+                
+            </ul>
+           {tasks.length > 0 ? (
+               <button
+               onClick={() => {
+                   setTasks([]);
+                
+               }}
+               >Complete all tasks</button>
+           ) : (<div />)
+           }
 
             
         </div>
